caeabf8ef15eccec077a36ffdfb2289d1bfcfec1
george kyriakou <gi_or_gosk@hotmail.com>
2019-09-26 20:12:59 +0200

    update generate_questions.js

--- generate_questions.js
+++ generate_questions.js
@@ -1,22 +1,34 @@
-const compare_tamplates = require('./TEMPLATES/compare.json')
-const metadata = require('./metadata.json')
+const compare_tamplates = require("./TEMPLATES/compare.json");
+const metadata = require("./metadata.json");
+const { combineAll } = require("./helpers/combineAll.js");
 
-  String.prototype.replaceAll = function(search, replacement) {
-    return this.split(search).join(replacement);
+String.prototype.replaceAll = function(search, replacement) {
+  return this.split(search).join(replacement);
 };
-// const regexForVariable = /\${(.*?)\}/gm
 
+const regexForVariable = /\${(.*?)\}/gm;
 
-compare_tamplates.forEach(template => {    
-    const paramNamesArray = template.params.map(p => p.name)
-    const paramTypes = template.params.map(p => p.type)
-    
-    template.text.forEach(sentence => {
-        let tmpSentence = ""
-        paramNamesArray.forEach( pname=> {
-            const word = 'Yolo'
-            sentence.replaceAll(pname, word)
-        })
-    })
+compare_tamplates.forEach(template => {
+  const paramNamesArray = template.params.map(p => p.name);
+  const paramTypes = template.params.map(p => p.type);
+
+  const paramTypeFromMetadata = paramTypes.map(type => {
+    return metadata.types[type];
+  });
+  const arrayOfWordCombinations = combineAll(...paramTypeFromMetadata);
+
+  arrayOfWordCombinations.forEach(wordCombination => {
+    wordCombination.forEach((word, index) => {
+      template.text.forEach(sentence => {
+        const variableToReplace = paramNamesArray[index];
+        //search in sentence with 'variableToReplace', using 'regexForVariable',  and replace with word
+        //push sentence in "global" array of sentences
+        //repeat for next template
+      });
+    });
+  });
 });
 
+function checkAndReplace(sentence, word) {
+  //if word !exists sentence => replace
+}
