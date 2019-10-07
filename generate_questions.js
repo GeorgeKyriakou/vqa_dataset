c05d2c8cc2d74b410393a0d63e2de5e920e7106c
Angelos Papangelis <papangelisang@gmail.com>
2019-10-07 17:58:08 +0200

    Replace only the first regex with meta data

--- generate_questions.js
+++ generate_questions.js
@@ -1,50 +1,63 @@
-const fs = require("fs");
+
+////////////////////////////////////////////////
 
 const compare_tamplates = require("./TEMPLATES/compare.json");
 const metadata = require("./metadata.json");
 const { combineAll } = require("./helpers/combineAll.js");
 
-let questionsArray = [];
-compare_tamplates.forEach(template => {
-  const paramNamesArray = template.params.map(p => p.name);
-  const paramTypes = template.params.map(p => p.type);
-
-  const paramTypeFromMetadata = paramTypes.map(type => {
-    return metadata.types[type];
-  });
-  const arrayOfWordCombinations = combineAll(...paramTypeFromMetadata);
-
-  arrayOfWordCombinations.forEach(wordCombination => {
-    wordCombination.forEach((word, index) => {
-      template.text.forEach(sentence => {
-        try {
-          const variableToReplace = paramNamesArray[index];
-          const regEx = new RegExp(variableToReplace, "gm");
-          const updatedSentence = sentence.replace(regEx, word);
-          if (regEx.test(sentence)) {
-            questionsArray.push(updatedSentence);
-          } else {
-            console.info(`Could not match ${variableToReplace} in sentence`);
-          }
-        } catch (e) {
-          console.error("ERROR", e);
-        }
-      });
-    });
-  });
-});
-
-var file = fs.createWriteStream(
-  `./generated_questions/combination_questions.txt`
-);
-file.on("error", err => {
-  console.error("Error while writing to file:", err);
-});
-questionsArray.forEach(v => {
-  file.write(v + "\n");
-});
-file.end();
-
 String.prototype.replaceAll = function(search, replacement) {
   return this.split(search).join(replacement);
 };
+
+const regexForVariable = /\<(.*?)\>/gm;
+
+compare_tamplates.forEach(template => { 
+  template.text.forEach(sentence => {
+    regex = sentence.match(regexForVariable).map(s => `<${s.split('')[1]}>`)
+    const paramTypeFromMetadata = regex.map((name) => {
+      const p = template.params.find(p=> p.name === name)
+      return p && metadata.types[p.type];
+    });
+    try {
+      const arrayOfWordCombinations = combineAll(...paramTypeFromMetadata);
+      arrayOfWordCombinations.forEach((wordCombination)=> {
+        const combinationOfUniqueWords = [...new Set(wordCombination)]
+
+        if(regex.length === combinationOfUniqueWords.length) {   
+          let temp = sentence      
+          for(let i= 0; i < regex.length; i++ ) {
+            temp = sentence.replace(regex[i], wordCombination[i])      
+          }
+          console.log(temp);
+        }
+      })
+    } catch (e) {
+      console.error('ERROR for:', paramTypeFromMetadata)
+      console.error(e)
+    }
+  
+  });
+});
+
+
+function checkAndReplace(sentence, word) {
+  //if word !exists sentence => replace
+}
+
+
+
+
+     // wordCombination.forEach((word, index) => {
+        // const variableToReplace = paramNamesArray[index];
+        // regex = sentence.match(regexForVariable).map(s => `<${s.split('')[1]}>`)
+        // console.log(regex)
+
+
+        // regex.forEach(symbol => {
+
+          // if (symbol === variableToReplace) {
+            // finalSentence = sentence.replace(regexForVariable, word);
+            // console.log(finalSentence);
+          // }
+      // });
+      // });
