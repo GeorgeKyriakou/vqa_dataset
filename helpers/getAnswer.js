cf429ab634cfa0b0a546ce4ac4d8c5fed10a0f54
george kyriakou <gi_or_gosk@hotmail.com>
2019-12-22 16:11:10 +0200

    wip

--- helpers/getAnswer.js
+++ helpers/getAnswer.js
@@ -1,25 +1,25 @@
 module.exports.getAnswer = (nodes, functions_map) => {
   try {
-    let answer = [];
+    let answer = "";
     nodes.forEach((node, index) => {
       if (index === 0) {
-        answer.push(node.type);
+        answer += `${node.type} `;
       } else {
         if (node.inputs.length === 0) {
-          answer.push("fork");
+          answer += "fork ";
         } else if (node.inputs.length > 1) {
-          answer.push("union");
-          answer.push(node.type);
+          answer += "union ";
+          answer += `${node.type} `;
         }
         if (node.side_inputs) {
           node.side_inputs.forEach(si => {
             const f = functions_map.find(fm => fm.name === si);
-            answer.push(f.function);
+            answer += `${f.function} `;
           });
         }
       }
     });
-    return answer;
+    return answer.trim();
   } catch (e) {
     throw new Error("FUCK!");
   }
